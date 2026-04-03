f2560664dd815413a7c11923b2d31bbc0c3e0996
李锦鸿 <mac@OOoo00O0oo00.local>
2026-04-03 12:08:50 +0800

    fix: build error：export const runtime = 'edge';

--- open-next.config.ts
+++ open-next.config.ts
@@ -2,7 +2,7 @@ import type { OpenNextConfig } from "@opennextjs/cloudflare";
 
 const config: OpenNextConfig = {
   default: {
-    runtime: "edge",
+    runtime: "node",
   },
   // 如果你有特定的缓存规则或路由需求，可以在这里添加
 };
